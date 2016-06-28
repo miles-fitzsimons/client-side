7fd795f427c9ce5c48b5dd39b0c7e659210cf78a
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-06-28 13:37:57 +1200

    added button to render cat data

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var xhr = require('xhr')
-var greeting = require('./views/greeting.hbs')
+// var xhr = require('xhr')
 var catHBS = require('./views/cats.hbs')
 
 // var endpoint = 'https://api.wheretheiss.at/v1/satellites'
 var endpoint = 'http://localhost:3000/cats'
 
-xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error("ERR::", err)
-  }
-  console.log(data.body)
-  var cats = JSON.parse(data.body)
-  var obj = {}
-  obj.cats = cats
-  console.log("AAAA::", obj)
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = catHBS(obj)
+$('#btn').click(function() {
+	// xhr.get(endpoint, function(err, data) {
+	// 	if(err) {
+	// 		console.log('ERR::', err)
+	// 	}
+	// 	var cats = JSON.parse(data.body)
+	// 	var obj = {cats:cats}
+	// 	// obj.cats = cats
+	// 	$('main').append(catHBS(obj))
+	// })
+	fetch(endpoint)
+		.then(function(result) {
+			console.log(result)
+			return result.json()
+		})
+		.then(function(result) {
+					var cats = result
+		var obj = {cats:cats}
+		// obj.cats = cats
+		$('main').append(catHBS(obj))
+		})
 })
